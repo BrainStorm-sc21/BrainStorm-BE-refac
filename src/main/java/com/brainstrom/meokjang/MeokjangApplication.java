4f0c829172acb963bfd1ddd802128934f6e0edc6
KangMin <dlrkdals421@ajou.ac.kr>
2023-04-13 15:23:11 +0900

    🎉Init Spring Boot Project

---
+++ src/main/java/com/brainstrom/meokjang/MeokjangApplication.java
@@ -0,0 +1,13 @@
+package com.brainstrom.meokjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeokjangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeokjangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brainstrom/meokjang/MeokjangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainstrom.meokjang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeokjangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
