5ca658c7eed679f38c6ad1582c8cc6048cdd1e9e
Hyeonji-Jung <welloff.jj@gmail.com>
2023-05-06 21:27:18 +0900

    ➕ Add 1.02, 2.02_add HTTP response status

--- src/main/java/com/brainstrom/meokjang/controller/UserController.java
+++ src/main/java/com/brainstrom/meokjang/controller/UserController.java
@@ -3,9 +3,10 @@ package com.brainstrom.meokjang.controller;
 import com.brainstrom.meokjang.domain.User;
 import com.brainstrom.meokjang.dto.LoginRequestDTO;
 import com.brainstrom.meokjang.dto.SignupRequestDTO;
-import com.brainstrom.meokjang.dto.UserDTO;
 import com.brainstrom.meokjang.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,28 +19,32 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users/create")
-    public String create(@RequestBody SignupRequestDTO dto, BindingResult result) {
+    public ResponseEntity<Object> create(@RequestBody SignupRequestDTO dto, BindingResult result) {
 
         if (result.hasErrors()) {
-            throw new IllegalStateException("잘못된 입력입니다.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result.getAllErrors());
         }
         try {
             User user = dto.toUser();
             userService.join(user);
             userService.validateDuplicateUser(dto.toUser());
+            return ResponseEntity.status(HttpStatus.CREATED).body(user);
         } catch (IllegalStateException e) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
-        return "redirect:/";
     }
 
     @PostMapping("/users/login")
-    public String login(@RequestBody LoginRequestDTO dto, BindingResult result) {
+    public ResponseEntity<Object> login(@RequestBody LoginRequestDTO dto, BindingResult result) {
 
         if (result.hasErrors()) {
-            throw new IllegalStateException("잘못된 입력입니다.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result.getAllErrors());
         }
-        userService.login(dto);
-        return "redirect:/";
+        try {
+            User user = userService.login(dto);
+            return ResponseEntity.ok(user);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
     }
 }
