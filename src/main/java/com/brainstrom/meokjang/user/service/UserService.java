7cd297eae09c7e4635a059cfd770b3474e468afe
Hyeonji-Jung <welloff.jj@gmail.com>
2023-06-02 17:17:14 +0900

    🚑️ Hotfix update username

--- src/main/java/com/brainstrom/meokjang/user/service/UserService.java
+++ src/main/java/com/brainstrom/meokjang/user/service/UserService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class UserService {
@@ -28,9 +31,17 @@ public class UserService {
     public UserInfoResponse updateUserInfo(Long userId, String userName) {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new IllegalStateException("존재하지 않는 유저입니다."));
-        user.changeUserName(userName);
-        User updateUser = userRepository.save(user);
-        return new UserInfoResponse(updateUser.getUserId(), updateUser.getUserName(), updateUser.getLocation(), updateUser.getLatitude(),
-                updateUser.getLongitude(), updateUser.getReliability(), updateUser.getStopUntil());
+        int result = userRepository.updateUserById(userId, userName);
+        if (result != 1) {
+            throw new IllegalStateException("유저 정보 수정에 실패하였습니다.");
+        }
+        User updateUser = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 유저입니다."));
+        return new UserInfoResponse(updateUser.getUserId(), userName, updateUser.getLocation(),
+                updateUser.getLatitude(), updateUser.getLongitude(), updateUser.getReliability(), updateUser.getStopUntil());
+    }
+
+    public List<User> getUserList() {
+        return userRepository.findAll();
     }
 }
