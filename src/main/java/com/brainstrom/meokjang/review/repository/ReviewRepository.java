2fac45d608162a2d55ecac596eeb80f49b25654b
Hyeonji-Jung <welloff.jj@gmail.com>
2023-06-09 20:56:17 +0900

    🔥 Delete unnecessary dependency

--- src/main/java/com/brainstrom/meokjang/review/repository/ReviewRepository.java
+++ src/main/java/com/brainstrom/meokjang/review/repository/ReviewRepository.java
@@ -1,7 +1,6 @@
 package com.brainstrom.meokjang.review.repository;
 
 import com.brainstrom.meokjang.review.domain.Review;
-import com.brainstrom.meokjang.review.dto.response.ReviewResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
